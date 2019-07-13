31a9ad83a88d9aa2da2e8f156c65637a750e5eb2
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-07-12 22:45:24 -0500

    Consumir servico de empleados

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,3 +1,4 @@
+import { EmpleadosService } from './empleados.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadosComponent implements OnInit {
 
-  constructor() { }
+  listaEmpleados = [];
+
+  constructor(private empleadoService: EmpleadosService) { }
 
   ngOnInit() {
+    this.listarEmpleados();
   }
 
+  listarEmpleados() {
+    this.listaEmpleados = [];
+
+    this.empleadoService.listarEmpleados().subscribe(
+      (response: any) => {
+        this.listaEmpleados = response.data;
+    });
+  }
 }
+
